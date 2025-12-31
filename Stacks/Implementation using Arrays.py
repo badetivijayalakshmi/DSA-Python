a45f386ad78386f1af67127c677de4c97b7c1490
Badeti Vijaya Lakshmi <badetivlakshmi18@gmail.com>
2025-12-31 13:43:55 +0530

    Implement stack class using arrays
    Implemented a stack class using an array with methods for push, pop, peek, isEmpty, and size. Added example usage of the stack with print statements.

---
+++ Stacks/Implementation using Arrays.py
@@ -0,0 +1,39 @@
+#Implementation using Arrays
+#Creation of a stack class
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self,element):
+        self.stack.append(element)
+
+    def pop(self):
+        if self.isEmpty():
+            return "Stack is empty"
+        return self.stack.pop()
+    
+    def peek(self):
+        if self.isEmpty():
+            return "Stack is empty"
+        return self.stack[-1]
+    
+    def isEmpty(self):
+        return len(self.stack) == 0
+
+    def size(self):
+        return len(self.stack)
+    
+my_stack = Stack()
+my_stack.push(10)
+my_stack.push(20)
+my_stack.push(30)
+my_stack.push(40)
+my_stack.push(50)
+print("Stack:",my_stack.stack)
+print("Pop:",my_stack.pop())
+print("New Stack:",my_stack.stack)
+print("Peek:",my_stack.peek())
+print("Empty or not:",my_stack.isEmpty())
+print("Size:",my_stack.size())
+#POP removes and returns the top element.
+#PEEK only returns the top element without removing it.
